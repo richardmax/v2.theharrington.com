cd82157abe71962557bc26f3150bcc0f3aecb399
richardmax <me@ricmax.com>
2014-06-11 16:26:38 +0100

    Fixed property pages (finally) + added map

--- v4/wp-content/themes/harrington_v1/single-properties.php
+++ v4/wp-content/themes/harrington_v1/single-properties.php
@@ -1,4 +1,14 @@
-<?php get_header(); ?>
+<?php 
+	get_header();
+	if( $post->post_parent != 0 ) {
+		// child page
+		$is_child = true;
+	
+	} else {
+		// top level page
+		$is_child = false;
+	}
+?>
 
 <nav class=" span2 secondaryNav">
     <ul class="nav nav-list">
@@ -11,241 +21,111 @@
             
 <nav class="propertyNav span4 available">
     <h1><?php the_title(); ?></h1>
-    
-	<?php
-		if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-			the_post_thumbnail();
-		}
-    ?>
-   
-    <ul id="my_cutom_type-list">
+    <ul id="gal" class="thumbnails nav">
 		<?php
-            $my_wp_query = new WP_Query();
-            $all_wp_pages = $my_wp_query->query(array('post_type' => 'properties'));
-            $children = get_page_children( $post->ID, $all_wp_pages );
-            // echo what we get back from WP to the browser
+            
+			if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                echo '<li class="props  active"><a href="" class="thumbnail">';
+                the_post_thumbnail('thumbnail');
+                echo '<figcaption>' . get_the_title() . '</figcaption></a></li>';
+            }
+        
+            $args = array(
+                'child_of'     => $post->ID,
+                'echo'         => 0,
+                'post_type'    => 'properties',
+                'post_status'  => 'publish',
+                'sort_column'  => 'menu_order',
+                'title_li'     => ''
+            );
+			
+            $children = get_pages($args); 
             //echo '<pre>' . print_r( $children, true ) . '</pre>';
-            $ID1 = $children[0]-> ID;
-            $ID2 = $children[1]-> ID;
-			
-			
-			
-			echo $ID1 . ", ";
-			echo $ID2;
-        ?> 
-    </ul>
-
+            
+            foreach ($children as $child) {
+                echo '<li class="props"><a href="?p=' . ($child -> ID) . '" class="thumbnail">';
+                echo get_the_post_thumbnail($child -> ID, 'thumbnail' );
+                echo ($child -> post_title);
+                echo '<figcaption>' . $child -> post_title . '</figcaption></a></li>';
+            }
+            
+        ?>
+	</ul>
 </nav> 
                        
 <article class="span6 prop contentarea">
     <section class="clearfix propImages row-fluid imagesToClone">
-    <?php
-        if ( get_post_gallery() ) :
-            $gallery = get_post_gallery( get_the_ID(), false );
-            /* Loop through all the image and output them one by one */
-            foreach( $gallery['src'] AS $src )
-            { ?>
-            	<img src="<?php echo $src; ?>" class="my-custom-class" alt="Gallery image" />
-       		<?php }
-        endif;
-	?>
-	<nav class="propContentNav">
-        <ul class="nav nav-tabs" id="myTab">
-            <li class="active"><a href="#desc" data-toggle="tab">Description</a></li>
-            <li class=""><a href="#spec" data-toggle="tab">Specifications</a></li>
-            <?php
-                if( $post->post_parent != 0 ) { // not top level ?>
-                    <li class=""><a href="#floorplans" data-toggle="tab">Floorplans</a></li>
-            <?php 
-                } else {
-                    // top level page ?>
-                    <li class=""><a href="#map" data-toggle="tab">Map</a></li>
-            <?php } ?>  
-    	</ul>
-	</nav>
-    <div class="tab-content">
-    	<div class="tab-pane active" id="desc">				
-        	<?php the_field('description'); ?>    
-        </div>
-        <div class="fade tab-pane" id="spec">
-            <?php the_field('specifications'); ?>
-        </div>
-         
-  		<?php if( $post->post_parent != 0 ) { // not top level ?>
-        	<div class="fade tab-pane" id="floorplans">
-            	<img src="<?php the_field('floorplan_image'); ?>" alt="" />
-                <br>
-                <a href="<?php the_field('floorplan_download'); ?>" >Download File</a>
-            </div>
-        <?php 
-			} else { // top level page ?>
-            <div class="fade tab-pane" id="map">
-            	<div class="acf-map">
+		<?php
+            if( $is_child == true ) { ?>
+                <h1 class="clearfix"> 1 Harrington Gardens</h1>
+        <?php } ?>
+		
+    	<ul class="thumbnails nav">
+			<?php
+                    $gallery = get_post_gallery( get_the_ID(), false );
+					//echo '<pre>' . print_r(  $gallery, true ) . '</pre>';
+					$galleryimages_ids = str_getcsv($gallery['ids']);
+
+					foreach( $galleryimages_ids AS $img_id ) { ?>
+                        <li class="imgs">
+                        	<?php 
+								$image_thumb_url = wp_get_attachment_thumb_url( $img_id );
+								$image_url = wp_get_attachment_url( $img_id );
+								$post_object = get_post( $img_id );        
+								$caption = $post_object->post_excerpt;
+							?>
+                            <a rel="1" href="<?php echo $image_url; ?>" class="thumbnail noselect ngg-fancybox fancybox">
+                                <img data-bigger="class=&quot;size-thumbnail" wp-image-64"="" alt="<?php echo $caption; ?>" src="<?php echo $image_thumb_url; ?>" width="150" height="150">
+                                <figcaption><?php echo $caption; ?></figcaption>
+                            </a>
+                        </li>	
+                    <?php } ?>
+        </ul>
+     
+        <nav class="propContentNav">
+            <ul class="nav nav-tabs" id="myTab">
+                <li class="active"><a href="#desc" data-toggle="tab">Description</a></li>
+                <li class=""><a href="#spec" data-toggle="tab">Specifications</a></li>
+                <?php
+                    if( $is_child == true ) { ?>
+                        <li class=""><a href="#floorplans" data-toggle="tab">Floorplans</a></li>
                 <?php 
-					$location = get_field('map');
-					if( !empty($location) ): ?>
-                        <div class="acf-map">
-                            <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-                        </div>
-				<?php endif; ?>
+                    } else { ?>
+                        <li class=""><a href="#map" data-toggle="tab">Map</a></li>
+                <?php } ?>  
+            </ul>
+        </nav>
+        
+        <div class="tab-content">
+            <div class="tab-pane active" id="desc">				
+                <?php the_field('description'); ?>    
             </div>
-            
-			<style type="text/css">
-				.acf-map {
-					width: 400px;
-					height: 400px;
-					border: #ccc solid 1px;
-					margin: 20px 0;
-				}
-			</style>
-            
-			<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
-            <script type="text/javascript">
-				(function($) {
-				 
-				/*
-				*  render_map
-				*
-				*  This function will render a Google Map onto the selected jQuery element
-				*
-				*  @type	function
-				*  @date	8/11/2013
-				*  @since	4.3.0
-				*
-				*  @param	$el (jQuery element)
-				*  @return	n/a
-				*/
-				 
-				function render_map( $el ) {
-					// var
-					var $markers = $el.find('.marker');
-				 
-					// vars
-					var args = {
-						zoom		: 16,
-						center		: new google.maps.LatLng(0, 0),
-						mapTypeId	: google.maps.MapTypeId.ROADMAP
-					};
-				 
-					// create map	        	
-					var map = new google.maps.Map( $el[0], args);
-				 
-					// add a markers reference
-					map.markers = [];
-					// add markers
-					$markers.each(function(){
-						add_marker( $(this), map );
-					});
-					
-					// center map
-					center_map( map );
-				}
-				 
-				/*
-				*  add_marker
-				*
-				*  This function will add a marker to the selected Google Map
-				*
-				*  @type	function
-				*  @date	8/11/2013
-				*  @since	4.3.0
-				*
-				*  @param	$marker (jQuery element)
-				*  @param	map (Google Map object)
-				*  @return	n/a
-				*/
-				 
-				function add_marker( $marker, map ) {
-				 
-					// var
-					var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
-				 
-					// create marker
-					var marker = new google.maps.Marker({
-						position	: latlng,
-						map			: map
-					});
-				 
-					// add to array
-					map.markers.push( marker );
-				 
-					// if marker contains HTML, add it to an infoWindow
-					if( $marker.html() )
-					{
-						// create info window
-						var infowindow = new google.maps.InfoWindow({
-							content		: $marker.html()
-						});
-				 
-						// show info window when marker is clicked
-						google.maps.event.addListener(marker, 'click', function() {
-							infowindow.open( map, marker );
-						});
-					}
-				 
-				}
-				 
-				/*
-				*  center_map
-				*
-				*  This function will center the map, showing all markers attached to this map
-				*
-				*  @type	function
-				*  @date	8/11/2013
-				*  @since	4.3.0
-				*
-				*  @param	map (Google Map object)
-				*  @return	n/a
-				*/
-				 
-				function center_map( map ) {
-				 
-					// vars
-					var bounds = new google.maps.LatLngBounds();
-				 
-					// loop through all markers and create bounds
-					$.each( map.markers, function( i, marker ){
-						var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
-						bounds.extend( latlng );
-				 
-					});
-				 
-					// only 1 marker?
-					if( map.markers.length == 1 )
-					{
-						// set center of map
-						map.setCenter( bounds.getCenter() );
-						map.setZoom( 16 );
-					}
-					else
-					{
-						// fit to bounds
-						map.fitBounds( bounds );
-					}
-				 
-				}
-				 
-				/*
-				*  document ready
-				*
-				*  This function will render each map when the document is ready (page has loaded)
-				*
-				*  @type	function
-				*  @date	8/11/2013
-				*  @since	5.0.0
-				*
-				*  @param	n/a
-				*  @return	n/a
-				*/
-				
-					$(document).ready(function(){
-						$('.acf-map').each(function(){
-							render_map( $(this) );
-						});
-					});
-				})(jQuery);
-			</script>    
-       </div>
+            <div class="fade tab-pane" id="spec">
+                <?php the_field('specifications'); ?>
+            </div>
+             
+            <?php if( $is_child == true ) { ?>
+                <div class="fade tab-pane" id="floorplans">
+                    <img src="<?php the_field('floorplan_image'); ?>" alt="" />
+                    <br>
+                    <a href="<?php the_field('floorplan_download'); ?>" >Download File</a>
+                </div>
+            <?php 
+                } else { ?>
+                <div class="fade tab-pane" id="map">
+                    <div class="acf-map">
+                    <?php 
+                        $location = get_field('map');
+                        if( !empty($location) ): ?>
+                            <div class="acf-map">
+                                <div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+                            </div>
+                    <?php endif; ?>
+                </div>
+                
+                <?php get_template_part('map') ?>
+                
+           </div>
    <?php } ?>
 </div>
 </section>		
