bc554d59dc5d43a3bfa21c2260f5b840a3fefe04
richardmax <me@ricmax.com>
2014-06-12 15:39:05 +0100

    Sorted about page

--- v4/wp-content/themes/harrington_v1/page-about.php
+++ v4/wp-content/themes/harrington_v1/page-about.php
@@ -9,13 +9,25 @@
     <?php wp_nav_menu(array('menu' => 'Side Menu')); ?>
 </nav>
 				
-<section class="span10">
-	<?php
-		while (have_posts()) : the_post();
-			//echo $query->post_content;
-			the_content();
-		endwhile; 
-	?>
-</section>
+<article class="span10">
+  <h1><?php the_title(); ?></h1>
+  <div class="tab_wrap">
+    <ul class="nav nav-tabs">
+      <li class="active"><a data-toggle="tab" href="#first">The Harrington</a></li>
+      <li class=""><a data-toggle="tab" href="#second">Our Exclusive Clients</a></li>
+    </ul>
+    <div class="tab-content">
+      <div class="tab-pane fade active in" id="first">
+        	<?php the_field('the_harrington'); ?>
+      </div>
+      <div class="tab-pane fade" id="second">
+        	<?php the_field('our_exclusive_clients'); ?>
+      </div>
+    </div>
+  </div>
+  
+  <?php get_template_part('carousel') ?>
+
+</article>
 
 <?php get_footer(); ?>
